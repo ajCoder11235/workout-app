2aeecab0161693ab8f8652eac2c60e3d1a482082
ajCoder11235 <ajaz030507@gmail.com>
2025-07-27 15:51:20 +0530

    Initial Commit

---
+++ sw.js
@@ -0,0 +1,56 @@
+// Define a cache name
+const CACHE_NAME = 'workout-planner-v1';
+
+// List of files to cache
+const urlsToCache = [
+  '/',
+  '/index.html',
+  // You would add your CSS and JS files here if they were separate
+  // '/style.css',
+  // '/app.js',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+// Install event: opens the cache and adds the core files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event: serves cached content when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // Not in cache - fetch from network
+        return fetch(event.request);
+      })
+  );
+});
+
+// Activate event: cleans up old caches
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
